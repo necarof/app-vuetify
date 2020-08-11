69c39d612fc5e47fd00e92eec85189cb60c82509
Nikita <43652463+necarof@users.noreply.github.com>
2020-08-11 13:21:49 +0600

    add toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,29 @@
 <template>
-  <h1>Hello</h1>
+  <v-app>
+    <v-navigation-drawer app temporary>
+      <!-- -->
+    </v-navigation-drawer>
+
+    <v-app-bar
+      dark
+      app
+      color="primary"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Vuetify Application</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn>
+        Link
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
