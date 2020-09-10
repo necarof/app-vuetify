1848980fde08a916ec2b8e15c410e52799ff5e0d
Nikita <43652463+necarof@users.noreply.github.com>
2020-09-10 11:10:50 +0600

    Editing an ad

--- src/store/ads.js
+++ src/store/ads.js
@@ -20,6 +20,14 @@ export default {
     },
     loadAds (state, payload) {
       state.ads = payload
+    },
+    updateAd (state, {title, description, id}) {
+      const ad = state.ads.find(a => {
+        return a.id === id
+      })
+
+      ad.title = title
+      ad.description = description
     }
   },
   actions: {
@@ -82,6 +90,25 @@ export default {
         }
         
 
+        commit('setLoading', false)
+      } catch (error) {
+        commit('setError', error.message)
+        commit('setLoading', false)
+        throw error
+      }
+    },
+
+    async updateAd ({commit}, {title, description, id}) {
+      commit('clearEroor')
+      commit('setLoading', true)
+
+      try {
+        await fb.database().ref('ads').child(id).update({
+          title, description
+        })
+        commit('updateAd', {
+          title, description, id
+        })
         commit('setLoading', false)
       } catch (error) {
         commit('setError', error.message)
